79f5d64a6d6220281d6d82008a7d5d403dfa92cb
amir <amirtaki623@gmail.com>
2025-05-22 20:56:14 +0330

    js : select filed onclick

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 var selectField = document.getElementById("selectField")
 var selectText = document.getElementById("selectText")
 var options = document.getElementsByClassName("options")
+var list = document.getElementById("list")
 
 
+selectField.onclick = function (){
+    list.classList.toggle('hide')
+}
 
 for(option of options){
     option.onclick  = function()  {
